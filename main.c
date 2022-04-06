2ce2997dee57e43d18227feb7d25e2d0cba96a95
smgarner229 <smgarner@berkeley.edu>
2022-04-05 19:58:40 -0700

    Swapping to OMP implimentation

--- main.c
+++ main.c
@@ -2,18 +2,19 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <mpi.h>
+//#include <mpi.h>
+#include <omp.h>
 
 int main(int argc, char ** argv)
 {
     int node;
     
-    MPI_Init(&argc,&argv);
-    MPI_Comm_rank(MPI_COMM_WORLD,&node);
+//    MPI_Init(&argc,&argv);
+//    MPI_Comm_rank(MPI_COMM_WORLD,&node);
 
-    printf("Hello from cpu %d\n",node);
+    printf("Hello from cpu %d\n",omp_get_thread_num());
 
-    MPI_Finalize();
+//    MPI_Finalize();
 
     return EXIT_SUCCESS;
 }
