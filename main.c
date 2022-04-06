2ec745d759e0d578e77a276dff28118d7420e35b
smgarner229 <smgarner@berkeley.edu>
2022-04-06 09:51:41 -0700

    New style of repository

--- main.c
+++ main.c
@@ -1,20 +1,12 @@
-#include <stddef.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-//#include <mpi.h>
-#include <omp.h>
 
 int main(int argc, char ** argv)
 {
     int node;
     
-//    MPI_Init(&argc,&argv);
-//    MPI_Comm_rank(MPI_COMM_WORLD,&node);
-
-    printf("Hello from cpu %d\n",omp_get_thread_num());
-
-//    MPI_Finalize();
+    printf("Hello there\n");
+    printf("General Kenobi\n");
 
     return EXIT_SUCCESS;
 }
