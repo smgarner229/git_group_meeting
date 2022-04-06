a6365559dac1e0959255f28bc1a827054bbb7c14
Scott Garner <smgarner@berkeley.edu>
2022-04-06 11:41:19 -0700

    New stuff in function2 in example.py

--- Python/example.py
+++ Python/example.py
@@ -2,7 +2,7 @@ def function_1():
     return "Does some stuff"
 
 def function_2():
-    return "Does some other stuff"
+    return "Does new stuff"
 
 if __name__ == "__main__":
     print(function_1())
