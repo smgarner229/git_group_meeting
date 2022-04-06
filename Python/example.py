172349e4bcabe39370cd635454b21884b5e5be2b
Scott Garner <41025073+smgarner229@users.noreply.github.com>
2022-04-06 11:40:12 -0700

    Update function2 in example.py
    Mostly for pulling to local repo testing.

--- Python/example.py
+++ Python/example.py
@@ -2,7 +2,7 @@ def function_1():
     return "Does some stuff"
 
 def function_2():
-    return "Does some other stuff"
+    return "Does the same stuff"
 
 if __name__ == "__main__":
     print(function_1())
